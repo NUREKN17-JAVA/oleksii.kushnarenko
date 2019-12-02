8d7fbeaa543156c9cbb1fede50780e80791c58d0
Alex-K123 <oleksii.kushnarenko@nure.ua>
2019-12-02 18:50:32 +0200

    Test for the main window

--- usermanagement/src/test/java/ua/nure/itkn179/kushnarenko/gui/MainFrameTest.java
+++ usermanagement/src/test/java/ua/nure/itkn179/kushnarenko/gui/MainFrameTest.java
@@ -2,22 +2,30 @@ package ua.nure.itkn179.kushnarenko.gui;
 
 import java.awt.Component;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
 import junit.extensions.jfcunit.JFCTestCase;
 import junit.extensions.jfcunit.JFCTestHelper;
 import junit.extensions.jfcunit.TestHelper;
 import junit.extensions.jfcunit.finder.NamedComponentFinder;
 
 public class MainFrameTest extends JFCTestCase {
-
-	   private MainFrame mainFrame;
-
-	@Override
-	    public void setUp() throws Exception {
-	        super.setUp();
-	        setHelper(new JFCTestHelper());
-	        mainFrame = new MainFrame();
-	        mainFrame.setVisible(true);
-	    }
+		private static final String DELETE_BUTTON = "deleteButton";
+		private static final String DETAILS_BUTTON = "detailsButton";
+		private static final String EDIT_BUTTON = "editButton";
+		private static final String ADD_BUTTON = "addButton";
+		private static final String BROWSE_PANEL = "browse Panel";
+	
+		private MainFrame mainFrame;
+	
+		@Override
+		public void setUp() throws Exception {
+			super.setUp();
+			setHelper(new JFCTestHelper());
+			mainFrame = new MainFrame();
+			mainFrame.setVisible(true);
+		}
 
 	    @Override
 	    public void tearDown() throws Exception {
@@ -35,5 +43,14 @@ public class MainFrameTest extends JFCTestCase {
 	    	assertNotNull("Could not find component'" + name + "'",component);
 	    	return component;
 	    }
+	    
+	    public void testBrowseControls() {
+			find(JPanel.class,BROWSE_PANEL);
+			//find user table
+			find (JButton.class, ADD_BUTTON);
+			find (JButton.class, EDIT_BUTTON);
+			find (JButton.class, DETAILS_BUTTON);
+			find (JButton.class, DELETE_BUTTON);		
+		}
 
 }
